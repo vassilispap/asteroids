cf900c4db8b9534f2361818636d9516a72da83cd
Papandoniou Vasileios <vassilis.pap@gmail.com>
2025-01-28 21:36:39 +0200

    Lot of debbuging to previous

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+#    print("Starting asteroids!")
+#    print(f"Screen width: {SCREEN_WIDTH}")
+#    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-#    print(check_init)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill(1, rect = (0, 400, 700, 300))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black", rect = (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.flip()
 
 
