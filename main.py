bb0635dea538758ac4fe396b8f7715e1423f675b
Papandoniou Vasileios <vassilis.pap@gmail.com>
2025-01-28 21:26:05 +0200

    First gameloop

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+#    print(check_init)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(1, rect = (0, 400, 700, 300))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
